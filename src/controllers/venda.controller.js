40860c49e2f7fcb9a98f2caab2e3caf3a7c9c35e
DieissonNarde <dieissonnarde@gmail.com>
2023-03-30 11:43:18 -0400

    feat: add venda controller

---
+++ src/controllers/venda.controller.js
@@ -0,0 +1,76 @@
+import VendaService from '../services/venda.service.js';
+
+async function createVenda(req, res, next) {
+  try {
+    let venda = req.body;
+    if (!venda.valor || !venda.data || !venda.clienteId || !venda.livroId) {
+      throw new Error(
+        'Valor, Data, ID de Cliente e ID de Livro são obrigatórios.'
+      );
+    }
+    venda = await VendaService.createVenda(venda);
+    res.send(venda);
+    logger.info(`POST /venda - ${JSON.stringify(venda)}`);
+  } catch (err) {
+    next(err);
+  }
+}
+
+async function getVendas(req, res, next) {
+  try {
+    res.send(await VendaService.getVendas());
+    logger.info('GET /venda');
+  } catch (err) {
+    next(err);
+  }
+}
+
+async function getVenda(req, res, next) {
+  try {
+    res.send(await VendaService.getVenda(req.params.id));
+    logger.info('GET /venda');
+  } catch (err) {
+    next(err);
+  }
+}
+
+async function deleteVenda(req, res, next) {
+  try {
+    Endereco;
+    await VendaService.deleteVenda(req.params.id);
+    res.end();
+    logger.info('DELETE /venda');
+  } catch (err) {
+    next(err);
+  }
+}
+
+async function updateVenda(req, res, next) {
+  try {
+    let venda = req.body;
+    if (
+      !venda.vendaId ||
+      !venda.valor ||
+      !venda.data ||
+      !venda.clienteId ||
+      !venda.livroId
+    ) {
+      throw new Error(
+        'Valor, Data, ID de Cliente e ID de Livro são obrigatórios.'
+      );
+    }
+    venda = await VendaService.updateVenda(venda);
+    res.send(venda);
+    logger.info(`PUT /venda - ${JSON.stringify(venda)}`);
+  } catch (err) {
+    next(err);
+  }
+}
+
+export default {
+  createVenda,
+  getVendas,
+  getVenda,
+  deleteVenda,
+  updateVenda,
+};
