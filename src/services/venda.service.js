642532aeb466b26a72cc3a7b7d2e1e8909efa6fb
DieissonNarde <dieissonnarde@gmail.com>
2023-03-29 23:56:27 -0400

    feat: add venda service

---
+++ src/services/venda.service.js
@@ -0,0 +1,29 @@
+import VendaRepository from '../repositories/venda.repository.js';
+
+async function createVenda(venda) {
+  return await VendaRepository.insertVenda(venda);
+}
+
+async function updateVenda(venda) {
+  return await VendaRepository.updateVenda(venda);
+}
+
+async function deleteVenda(id) {
+  await VendaRepository.deleteVenda(id);
+}
+
+async function getVendas() {
+  return await VendaRepository.getAnimais();
+}
+
+async function getVenda(id) {
+  return await VendaRepository.getVenda(id);
+}
+
+export default {
+  createVenda,
+  getVendas,
+  getVenda,
+  deleteVenda,
+  updateVenda,
+};
