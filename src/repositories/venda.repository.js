8143527a9d08f2e21ceca706c79f53b0d1d1472c
DieissonNarde <dieissonnarde@gmail.com>
2023-03-29 23:25:47 -0400

    feat: add venda repository

---
+++ src/repositories/venda.repository.js
@@ -0,0 +1,59 @@
+import Venda from '../models/venda.model.js';
+
+async function insertVenda(venda) {
+  try {
+    return await Venda.create(venda);
+  } catch (err) {
+    throw err;
+  }
+}
+
+async function updateVenda(venda) {
+  try {
+    await Venda.update(venda, {
+      where: {
+        vendaId: venda.vendaId,
+      },
+    });
+
+    return await getVenda(venda.vendaId);
+  } catch (err) {
+    throw err;
+  }
+}
+
+async function deleteVenda(id) {
+  try {
+    return await Venda.destroy({
+      where: {
+        vendaId: id,
+      },
+    });
+  } catch (err) {
+    throw err;
+  }
+}
+
+async function getVendas() {
+  try {
+    return await Venda.findAll();
+  } catch (err) {
+    throw err;
+  }
+}
+
+async function getVenda(id) {
+  try {
+    return await Venda.findByPk(id, { raw: true });
+  } catch (err) {
+    throw err;
+  }
+}
+
+export default {
+  insertVenda,
+  getVendas,
+  getVenda,
+  updateVenda,
+  deleteVenda,
+};
