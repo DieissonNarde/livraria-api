0fa6acefaf93b79f5998782875f8983c50245249
DieissonNarde <dieissonnarde@gmail.com>
2023-03-30 18:09:07 -0400

    feat: add livro info in livro service

--- src/services/livro.service.js
+++ src/services/livro.service.js
@@ -1,4 +1,5 @@
 import LivroRepository from '../repositories/livro.repository.js';
+import LivroInfoRepository from '../repositories/livroInfo.repository.js';
 
 async function createLivro(livro) {
   return await LivroRepository.insertLivro(livro);
@@ -20,10 +21,35 @@ async function getLivro(id) {
   return await LivroRepository.getLivro(id);
 }
 
+async function createLivroInfo(livroInfo) {
+  await LivroInfoRepository.createLivroInfo(livroInfo);
+}
+
+async function updateLivroInfo(livroInfo) {
+  await LivroInfoRepository.updateLivroInfo(livroInfo);
+}
+
+async function deleteLivroInfo(livroId) {
+  await LivroInfoRepository.deleteLivroInfo(livroId);
+}
+
+async function createAvaliacao(avaliacao, livroId) {
+  await LivroInfoRepository.createAvaliacao(avaliacao, livroId);
+}
+
+async function deleteAvaliacao(livroId, index) {
+  await LivroInfoRepository.deleteAvaliacao(parseInt(livroId), index);
+}
+
 export default {
   createLivro,
   getLivros,
   getLivro,
   deleteLivro,
   updateLivro,
+  createLivroInfo,
+  updateLivroInfo,
+  deleteLivroInfo,
+  createAvaliacao,
+  deleteAvaliacao,
 };
